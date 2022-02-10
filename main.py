d102f6c5ae9ab4956364fdaa856ff1872688ea48
Stepan <nasluzov.stepan@gmail.com>
2022-02-10 23:02:08 +0300

    1.0.05 - add resources

--- main.py
+++ main.py
@@ -1,37 +1,50 @@
 import pygame
 import os
+SIDE = 21
+FPS = 30
 
 
 class Worker(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        # self.image = .subsurface((0, 0), (40, 40))
+        self.rect = pygame.Rect(0, 0, 40, 40)
+        self.rect.x = cur_x * 40
+        self.rect.y = cur_y * 40
         self.image = cur_image.subsurface((0, 0), (40, 40))
-        self.rect = self.image.get_rect()
-        self.rect.x = cur_y * 40
-        self.rect.y = cur_x * 40
-        self.image = cur_image.subsurface((0, 0), (40, 40))
+        self.frames = [[], [], [], []]
+        self.cut_sheet(cur_image, 4, 5)
+        self.image = self.frames[0][0]
+
+    def cut_sheet(self, sheet, columns, rows):
+        for j in range(columns):
+            for i in range(rows):
+                self.frames[j].append(sheet.subsurface((j * 40, i * 40), (40, 40)))
+
+    def new_pos(self, y, x):
+        self.rect.x = x * 40
+        self.rect.y = y * 40
 
     def update(self, direction):
         dx = 0
         dy = 0
         if direction == 0:
-            dy = 1
+            dy = 4
         elif direction == 1:
-            dx = 1
+            dx = 4
         elif direction == 2:
-            dx = -1
+            dx = -4
         elif direction == 3:
-            dy = -1
-        for i in range(1, 41, 1):
+            dy = -4
+        for i in range(1, 11):
             self.rect.y += dy
             self.rect.x += dx
             dir_num = i % 5
-            self.image = cur_image.subsurface((direction * 40, dir_num * 40), (40, 40))
+            self.image = self.frames[direction][dir_num]
             draw(screen)
             all_sprites.draw(screen)
             pygame.display.flip()
-            pygame.time.wait(80)
-            all_sprites.draw(screen)
+            pygame.time.wait(75)
 
 
 def load_image(name, colorkey=None):
@@ -54,25 +67,24 @@ def draw(scr):
     pygame.draw.rect(scr, (180, 0, 0), ((840, 0), (160, 840)))
     for y in range(21):
         for x in range(21):
-            if pos[y][x] == 'W':
+            if pos[y][x] == '*':
+                pygame.draw.rect(scr, (0, 0, 150), ((x * 40, y * 40), (40, 40)))
+            elif pos[y][x] == 'W':
                 pygame.draw.rect(scr, (255, 255, 255), ((x * 40, y * 40), (40, 40)), 1)
-            elif pos[y][x] == 'B':
-                pygame.draw.circle(scr, (255, 255, 0), (40 * x + 20, 40 * y + 20), 20, 4)
             elif pos[y][x] == 'L':
                 pygame.draw.rect(scr, (0, 255, 100), ((40 * x + 1, 40 * y + 1), (38, 38)), 3)
-            elif pos[y][x] == 'P':
-                pygame.draw.circle(scr, (255, 0, 0), (40 * x + 20, 40 * y + 20), 20, 1)
-                pygame.draw.circle(scr, (0, 255, 100), (40 * x + 20, 40 * y + 20), 10)
+            elif pos[y][x] == ' ':
+                pygame.draw.rect(scr, (0, 0, 0), ((x * 40, y * 40), (40, 40)))
+            elif pos[y][x] == 'B':
+                pygame.draw.circle(scr, (255, 255, 0), (40 * x + 20, 40 * y + 20), 20, 4)
             elif pos[y][x] == 'X':
                 pygame.draw.rect(scr, (0, 255, 100), ((40 * x + 1, 40 * y + 1), (38, 38)), 3)
                 pygame.draw.circle(scr, (255, 255, 0), (40 * x + 20, 40 * y + 20), 20, 4)
-            elif pos[y][x] == ' ':
-                pygame.draw.rect(scr, (0, 0, 0), ((x * 40, y * 40), (40, 40)))
-            elif pos[y][x] == '*':
-                pygame.draw.rect(scr, (0, 0, 150), ((x * 40, y * 40), (40, 40)))
+            elif pos[y][x] == 'P':
+                pygame.draw.rect(scr, (0, 255, 100), ((40 * x + 1, 40 * y + 1), (38, 38)), 3)
 
 
-states = {121: [' ', 'H', 'W'], 122: [' ', 'H', ' '], 123: [' ', 'H', 'B'], 124: [' ', 'H', 'L'], 125: [' ', 'H', 'X'],
+STATES = {121: [' ', 'H', 'W'], 122: [' ', 'H', ' '], 123: [' ', 'H', 'B'], 124: [' ', 'H', 'L'], 125: [' ', 'H', 'X'],
           132: [' ', 'H', 'B'], 134: [' ', 'H', 'X'],
           141: [' ', 'P', 'W'], 142: [' ', 'P', ' '], 143: [' ', 'P', 'B'], 144: [' ', 'P', 'L'], 145: [' ', 'P', 'X'],
           152: [' ', 'P', 'B'], 154: [' ', 'P', 'X'],
@@ -81,129 +93,110 @@ states = {121: [' ', 'H', 'W'], 122: [' ', 'H', ' '], 123: [' ', 'H', 'B'], 124:
           241: ['L', 'P', 'W'], 242: ['L', 'P', ' '], 243: ['L', 'P', 'B'], 244: ['L', 'P', 'L'], 245: ['L', 'P', 'X'],
           252: ['L', 'P', 'B'], 254: ['L', 'P', 'X']}
 
-pos = [['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-       ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']]
-
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 1000, 840
     screen = pygame.display.set_mode(size)
-lvl_map = open('sokoban_levels_pack.txt', 'r')
-for i in range(1):
-    lvl = lvl_map.readline()
-for i in range(21):
-    for j in range(21):
-        pos[i][j] = lvl[i * 21 + j]
-side = 21
-for k in range(side ** 2):
-    cur_y = k // side
-    cur_x = k % side
-    if pos[cur_y][cur_x] == 'H' or pos[cur_y][cur_x] == 'P':
-        break
-all_sprites = pygame.sprite.Group()
-cur_image = load_image('Worker.png')
-worker = Worker()
-all_sprites.add(worker)
-clock = pygame.time.Clock()
-draw(screen)
-all_sprites.draw(screen)
-pygame.display.flip()
-fps = 30
-clock = pygame.time.Clock()
-while True:
-    state = ''
-    for i in range(side):
-        for j in range(side):
-            state += pos[i][j]
-    if 'B' not in state:
+    lvl_map = open('sokoban_levels_pack.txt', 'r')
+    for i in range(1):
         lvl = lvl_map.readline()
-        for i in range(21):
-            for j in range(21):
-                pos[i][j] = lvl[i * 21 + j]
-        draw(screen)
-        pygame.display.flip()
-        clock.tick(fps)
+    pos = [[' ' for i in range(21)] for j in range(21)]
 
-    for event in pygame.event.get():
-        for k in range(side ** 2):
-            cur_y = k // side
-            cur_x = k % side
-            if pos[cur_y][cur_x] == 'H' or pos[cur_y][cur_x] == 'P':
-                next_y = cur_y
-                next_x = cur_x
-                after_y = cur_y
-                after_x = cur_x
-                if pos[cur_y][cur_x] == 'H':
-                    level_state = 100
-                else:
-                    level_state = 200
-                break
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                next_y += 1
-                after_y += 2
-                worker_state = 0
-            if event.key == pygame.K_UP:
-                next_y -= 1
-                after_y -= 2
-                worker_state = 3
-            if event.key == pygame.K_LEFT:
-                next_x -= 1
-                after_x -= 2
-                worker_state = 2
-            if event.key == pygame.K_RIGHT:
-                next_x += 1
-                after_x += 2
-                worker_state = 1
+    for i in range(21):
+        for j in range(21):
+            pos[i][j] = lvl[i * 21 + j]
 
-            if pos[next_y][next_x] == 'W':
-                level_state += 10
-            elif pos[next_y][next_x] == ' ':
-                level_state += 20
-            elif pos[next_y][next_x] == 'B':
-                level_state += 30
-            elif pos[next_y][next_x] == 'L':
-                level_state += 40
-            elif pos[next_y][next_x] == 'X':
-                level_state += 50
+    # for k in range(SIDE ** 2):
+    #     cur_y = k // SIDE
+    #     cur_x = k % SIDE
+    #     if pos[cur_y][cur_x] == 'H' or pos[cur_y][cur_x] == 'P':
+    #         break
 
-            if pos[after_y][after_x] == 'W':
-                level_state += 1
-            elif pos[after_y][after_x] == ' ':
-                level_state += 2
-            elif pos[after_y][after_x] == 'B':
-                level_state += 3
-            elif pos[after_y][after_x] == 'L':
-                level_state += 4
-            elif pos[after_y][after_x] == 'X':
-                level_state += 5
-
-            if level_state in states.keys():
-                worker.update(worker_state)
-                pos[cur_y][cur_x], pos[next_y][next_x], pos[after_y][after_x] = states[level_state]
+    all_sprites = pygame.sprite.Group()
+    cur_image = load_image('Worker.png')
+    worker = Worker()
+    all_sprites.add(worker)
+    clock = pygame.time.Clock()
     draw(screen)
     all_sprites.draw(screen)
     pygame.display.flip()
-    clock.tick(fps)
+    while True:
+        state = ''
+        for i in range(SIDE):
+            for j in range(SIDE):
+                state += pos[i][j]
+        if 'B' not in state:
+            lvl = lvl_map.readline()
+            for i in range(21):
+                for j in range(21):
+                    pos[i][j] = lvl[i * 21 + j]
+                    if pos[i][j] == 'H' or pos[i][j] == 'P':
+                        worker.new_pos(i, j)
+            draw(screen)
+            pygame.display.flip()
+            # clock.tick(FPS)
+
+        for event in pygame.event.get():
+            for k in range(SIDE ** 2):
+                cur_y = k // SIDE
+                cur_x = k % SIDE
+                if pos[cur_y][cur_x] == 'H' or pos[cur_y][cur_x] == 'P':
+                    next_y = cur_y
+                    next_x = cur_x
+                    after_y = cur_y
+                    after_x = cur_x
+                    if pos[cur_y][cur_x] == 'H':
+                        level_state = 100
+                    else:
+                        level_state = 200
+                    break
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    next_y += 1
+                    after_y += 2
+                    worker_state = 0
+                elif event.key == pygame.K_UP:
+                    next_y -= 1
+                    after_y -= 2
+                    worker_state = 3
+                elif event.key == pygame.K_LEFT:
+                    next_x -= 1
+                    after_x -= 2
+                    worker_state = 2
+                elif event.key == pygame.K_RIGHT:
+                    next_x += 1
+                    after_x += 2
+                    worker_state = 1
+
+                if pos[next_y][next_x] == 'W':
+                    level_state += 10
+                elif pos[next_y][next_x] == ' ':
+                    level_state += 20
+                elif pos[next_y][next_x] == 'B':
+                    level_state += 30
+                elif pos[next_y][next_x] == 'L':
+                    level_state += 40
+                elif pos[next_y][next_x] == 'X':
+                    level_state += 50
+
+                if pos[after_y][after_x] == 'W':
+                    level_state += 1
+                elif pos[after_y][after_x] == ' ':
+                    level_state += 2
+                elif pos[after_y][after_x] == 'B':
+                    level_state += 3
+                elif pos[after_y][after_x] == 'L':
+                    level_state += 4
+                elif pos[after_y][after_x] == 'X':
+                    level_state += 5
+
+                if level_state in STATES.keys():
+                    worker.update(worker_state)
+                    pos[cur_y][cur_x], pos[next_y][next_x], pos[after_y][after_x] = STATES[level_state]
+        draw(screen)
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
 print('Победа!')
